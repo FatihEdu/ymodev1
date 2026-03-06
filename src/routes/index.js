e82cc890a22208bc38094fe5af8eef4b87f4870b
Bugra <ogemisi39@gmail.com>
2026-03-06 22:07:35 +0300

    mesaj

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,8 @@ const router = Router();
 
 router.get('/', (req, res) => {
     res.json({ status: 'ok', message: 'Express Jobs ile Asal Sayı Testleri API çalışıyor.' });
-    router.get('/health', (req, res) => {
+
+  router.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 
 });
